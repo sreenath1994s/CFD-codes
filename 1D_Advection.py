0db5a383039572120b63107b04ccc2393858ab28
Sreenath Subramaniam <sreenaths1994@outlook.com>
2020-05-12 14:23:48 +0900

    1D Non-linear convection

--- 1D_Advection.py
+++ 1D_Advection.py
@@ -5,6 +5,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import viz_tools
 
 # Domain Parameters and Physical Properties
 
@@ -20,7 +21,7 @@ dx = L_x / (N_x - 1)           # Spacing between grid points
 
 x  = np.linspace(0, L_x, N_x)  # Array with x points
 
-nt = 20                        # No: of time steps
+nt = 100                        # No: of time steps
 
 sigma = 0.2
                                
@@ -46,11 +47,11 @@ for t in range(nt):
 
 #       u2[i] = u1[i] - c * dt / dx * (u1[i] - u1[i-1])       # 1D Linear Convection
 
-#       u2[i] = u1[i] - u1[i] * dt / dx * (u1[i] - u1[i-1])   # 1D Non Linear Convection
+        u2[i] = u1[i] - u1[i] * dt / dx * (u1[i] - u1[i-1])   # 1D Non Linear Convection
 
-        u2[i] = u1[i] + nu * dt / (dx**2) * (u1[i+1] - 2*u1[i] + u1[i-1])   # 1D pure diffusio, central difference approximation for the second order spatial derivative.
+#        u2[i] = u1[i] + nu * dt / (dx**2) * (u1[i+1] - 2*u1[i] + u1[i-1])   # 1D pure diffusio, central difference approximation for the second order spatial derivative.
 
-        u_anim[t][i] = u2[i]
+        u_anim[t][i] = u1[i]
 
     u1 = u2.copy()
 
@@ -60,6 +61,8 @@ for t in range(nt):
     
 
 print( "Simulation Finished.. Visualizing data")
+
+eta_anim = viz_tools.u_animation(x, u_anim)
 
 fig, ax = plt.subplots()
 
---
+++ viz_tools.py
@@ -0,0 +1,43 @@
+##### Animation routine #####
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import os, sys
+
+
+# MPEG code path
+
+ff_path = os.path.join('C:/', 'Program Files (x86)/ffmpeg/bin/', 'ffmpeg.exe')
+plt.rcParams['animation.ffmpeg_path'] = ff_path
+if ff_path not in sys.path: sys.path.append(ff_path)
+plt.style.use('seaborn-pastel')
+
+def u_animation(x , u_anim):
+
+    fig, ax = plt.subplots(1, 1)
+    ax = plt.axes(xlim=(0, 2), ylim=(0, 2)) 
+    plt.xlabel("x [m]",   fontname = "serif", fontsize = 10)
+    plt.ylabel("u [m/s]", fontname = "serif", fontsize = 10)
+    line1, = ax.plot([], [], lw=2)  
+    line2, = ax.plot(x, u_anim[0][:].flatten(), lw=2)   # initial condition
+
+    def init():
+        
+	    line1.set_data([], []) 
+	    return line1,
+
+    def update(t):
+        ax.set_title(str("t_step = " + str(t)), fontname = "serif", fontsize = 12)
+        u_out = u_anim[t][:].flatten()
+        x_out = x.flatten()
+        line1.set_data(x_out, u_out)
+        return line1,
+
+    anim = animation.FuncAnimation(fig, update, init_func=init, frames=len(u_anim), interval=1.2, blit=True) 
+
+    mpeg_writer = animation.FFMpegWriter(fps = 24, bitrate = 10000, codec = "libx264", extra_args = ["-pix_fmt", "yuv420p"])
+
+    anim.save('final.mp4', writer=mpeg_writer)
+
+    return anim
